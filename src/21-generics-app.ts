4c0110d656174ebcd542fd1b78b6488e9c8d380e
Roberto Gomez <RobertoGo@protonmail.com>
2021-04-05 11:54:49 +0200

    Generics and Overloads - Function Generics

---
+++ src/21-generics-app.ts
@@ -0,0 +1,27 @@
+class Pizza {
+  constructor(private name: string, private price: number) {}
+}
+
+class List<T> {
+  private list: T[];
+
+  addItem(item: T): void {
+    this.list.push(item);
+  }
+
+  getList(): T[] {
+    return this.list;
+  }
+}
+
+const list = new List<Pizza>();
+list.addItem(new Pizza("Pepperoni", 15));
+
+const pizzas: Pizza[] = list.getList();
+
+class Coupon {
+  constructor(private name: string) {}
+}
+
+const anotherList = new List<Coupon>();
+anotherList.addItem(new Coupon("PIZZA25"));
